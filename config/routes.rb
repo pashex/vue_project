415225efd8da87847bad56a9c8c6943a641e4393
Pavel Golenkov <pgolenkov@gmail.com>
2020-09-15 10:50:30 +0500

    Add clients and staffs Vue applications

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :clients
   devise_for :staffs
+
+  resources :clients, only: :index
+  resources :staffs, only: :index
+
+  root to: 'application#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
