04779a5fd1c1aa0360de1e7dac91f3868d831f80
Pavel Golenkov <pgolenkov@gmail.com>
2020-10-25 12:23:38 +0500

    Add navbar component without api

---
+++ app/javascript/staffs/components/navbar.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  div(class="navbar")
+    img(:src="logo")
+    template(v-if="current_user")
+      div(class="current-user") {{ current_user }}
+      button(@click="logout") Exit
+    template(v-else)
+      p No user logged in
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: ['logo', 'current_user'],
+  methods: {
+    logout () {
+      console.log('logout')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- app/javascript/staffs/staffs.vue
+++ app/javascript/staffs/staffs.vue
@@ -1,13 +1,22 @@
 <template lang="pug">
-  p {{ message }}
+  div
+    navbar(:logo="logo" :current_user="current_user")
+    p {{ message }}
 </template>
 
 <script>
+import Navbar from './components/navbar.vue'
+
 export default {
   data: function () {
     return {
-      message: "Staffs list"
+      message: "Staffs list",
+      current_user: 'Test user',
+      logo: 'assets/logo.png'
     }
+  },
+  components: {
+    Navbar
   }
 }
 </script>
